2cb9a1e5101c55242db15937068ac08dc77f58b5
Josué Martell <josue.martell17@gmail.com>
2022-10-27 13:48:21 -0500

    feat: added alpha

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -69,6 +69,7 @@ return packer.startup(function(use)
 	use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
 	use { "RRethy/vim-illuminate", commit = "a2e8476af3f3e993bb0d6477438aad3096512e42" }
 	use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
+	use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
 
 	-- Snippets
 	use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" }
