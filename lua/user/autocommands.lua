1ceafd3d47a471018f6269864271bd4564663305
Josué Martell <josue.martell17@gmail.com>
2022-11-08 18:48:56 -0600

    fix: removed unnecessary autocommand and replace it with an option

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -42,13 +42,6 @@ vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
 	end,
 })
 
--- Fix autocomment
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-	callback = function()
-		vim.cmd("set formatoptions-=cro")
-	end,
-})
-
 -- Highlight yanked text
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	callback = function()
