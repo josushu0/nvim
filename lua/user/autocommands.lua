ee6196955bf498b232e758b5fbe0efef083d3c5e
Josué Martell <josue.martell17@gmail.com>
2022-11-30 15:30:14 -0600

    refactor: combine identical autocommands

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -9,18 +9,9 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
--- Set wrap and spell for gitcommit
+-- Set wrap and spell for gitcommit and markdown
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "gitcommit" },
-	callback = function()
-		vim.opt_local.wrap = true
-		vim.opt_local.spell = true
-	end,
-})
-
--- Set wrap and spell for markdown
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "markdown" },
+	pattern = { "gitcommit", "markdown" },
 	callback = function()
 		vim.opt_local.wrap = true
 		vim.opt_local.spell = true
