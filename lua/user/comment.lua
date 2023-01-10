f4c328583786933a94cfd680c4374ab3fa52e162
Josué Martell <josue.martell17@gmail.com>
2023-01-10 01:42:27 -0600

    fix: configured ts_context_commentstring correctly

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -17,7 +17,7 @@ comment.setup({
 	},
 	hooks = {
 		pre = function()
-			require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+			require("ts_context_commentstring.internal").update_commentstring()
 		end,
 	},
 })
