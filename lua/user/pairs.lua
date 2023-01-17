ed8b210c7544a24a6f67c5d4593729fe9c4b37ad
Josué Martell <josue.martell17@gmail.com>
2023-01-17 14:25:10 -0600

    feat: remove user folder

--- lua/user/pairs.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, pairs = pcall(require, "mini.pairs")
-if not status_ok then
-	return
-end
-
-pairs.setup({
-	modes = { insert = true, command = true, terminal = true },
-})
--- lua/user/sessions.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, sessions = pcall(require, "mini.sessions")
-if not status_ok then
-	return
-end
-
-sessions.setup()
