068490cbfb000bd90f090648adebe63ed1d039d2
Josué Martell <josue.martell17@gmail.com>
2023-01-17 22:56:30 -0600

    refactor: added cjs extension

--- lua/lsp/settings/tailwindcss.lua
+++ lua/lsp/settings/tailwindcss.lua
@@ -1,3 +1,3 @@
 return {
-	root_dir = require("lspconfig").util.root_pattern("tailwind.config.js", "tailwind.config.ts"),
+	root_dir = require("lspconfig").util.root_pattern("tailwind.config.js", "tailwind.config.ts", "tailwind.config.cjs"),
 }
